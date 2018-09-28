e22c90f6efb82dd398612c3fe1c794e90ed6952f
juliusbrose <julius.brose@zedat.fu-berlin.de>
2018-09-28 19:01:01 +0200

    modified middleware

--- Template/Server/Server/TCP/TCPMiddleware.java
+++ Template/Server/Server/TCP/TCPMiddleware.java
@@ -1,24 +1,28 @@
 package Server.TCP;
 
 import Server.Common.Middleware;
+import Server.Common.ResourceManager;
 import Server.Interface.IResourceManager;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.Member;
 import java.net.*;
 
 public class TCPMiddleware extends Middleware {
 
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         // Create the TCP server entry
       /*  if (args.length < 1) {
             System.out.println("Usage: java TCPMiddleware middleware server1, server 2, server3");
             System.exit(1);
         }*/
+      Middleware middleware = new Middleware("middleware");
         try {
             Socket flights = new Socket(args[0], 6111);
             System.out.println("Connected to flights");
@@ -36,62 +40,144 @@ public class TCPMiddleware extends Middleware {
                     String js = bis.readLine();
                     OutputStream os;
                     JSONObject jsob = new JSONObject(js);
-                    switch (jsob.getString("methodName")) {
-                        case "AddFlight":
-                            os = flights.getOutputStream();
-                            os.write(js.getBytes());
+                    String method = (jsob.getString("methodName"));
+                    if (method.contains("Flight")) {
+                        os = flights.getOutputStream();
+                        PrintWriter pw = new PrintWriter(os);
+                        pw.println(js);
+                        pw.flush();
+                        bis = new BufferedReader(new InputStreamReader(flights.getInputStream()));
+                        js = bis.readLine();
+                        os = clientSocket.getOutputStream();
+                        pw = new PrintWriter(os);
+                        pw.println(js);
+                        pw.flush();
+                    } else if (method.contains("Car")) {
+                        os = cars.getOutputStream();
+                        PrintWriter pw = new PrintWriter(os);
+                        pw.println(js);
+                        pw.flush();
+                        bis = new BufferedReader(new InputStreamReader(cars.getInputStream()));
+                        js = bis.readLine();
+                        os = clientSocket.getOutputStream();
+                        pw = new PrintWriter(os);
+                        pw.println(js);
+                        pw.flush();
+                    } else if (method.contains("Room")) {
+                        os = rooms.getOutputStream();
+                        PrintWriter pw = new PrintWriter(os);
+                        pw.println(js);
+                        pw.flush();
+                        bis = new BufferedReader(new InputStreamReader(rooms.getInputStream()));
+                        js = bis.readLine();
+                        os = clientSocket.getOutputStream();
+                        pw = new PrintWriter(os);
+                        pw.println(js);
+                        pw.flush();
+                    } else if (method.equals("bundle")){
+                        JSONArray flightnumbers = jsob.getJSONArray("flightnumbers");
+                        for (int i = 0; i < flightnumbers.length(); i++){
+                            Socket bundledFlights = new Socket(args[0], 6111);
+                            OutputStream outputStream = bundledFlights.getOutputStream();
+                            JSONObject flightJson = new JSONObject();
+                            flightJson.put("xid", jsob.getInt("xid"));
+                            flightJson.put("customerId", jsob.getInt("customerId"));
+                            flightJson.put("flightNumber", jsob.getInt(Integer.toString(i)));
+                            outputStream.write(method.getBytes());
+                            outputStream.flush();
+                            outputStream.close();
+                        }
+                        if(jsob.getBoolean("car")){
+                            os = cars.getOutputStream();
+                            JSONObject roomObject = new JSONObject();
+                            roomObject.put("xid", jsob.getInt("xid"));
+                            roomObject.put("customerId", jsob.getInt("customerId"));
+                            roomObject.put("location", jsob.getString("location"));
+                            os.write(roomObject.toString().getBytes());
                             os.flush();
                             os.close();
-                            bis = new BufferedReader(new InputStreamReader(flights.getInputStream()));
+                            bis = new BufferedReader(new InputStreamReader(cars.getInputStream()));
                             js = bis.readLine();
                             os = clientSocket.getOutputStream();
                             os.write(js.getBytes());
                             os.flush();
                             os.close();
-                        case "AddCars":
-                            os = cars.getOutputStream();
+                        }
+                        if(jsob.getBoolean("room")){
+                            os = rooms.getOutputStream();
+                            JSONObject roomObject = new JSONObject();
+                            roomObject.put("xid", jsob.getInt("xid"));
+                            roomObject.put("customerId", jsob.getInt("customerId"));
+                            roomObject.put("location", jsob.getString("location"));
+                            os.write(roomObject.toString().getBytes());
+                            os.flush();
+                            os.close();
+                            bis = new BufferedReader(new InputStreamReader(rooms.getInputStream()));
+                            js = bis.readLine();
+                            os = clientSocket.getOutputStream();
                             os.write(js.getBytes());
                             os.flush();
                             os.close();
-                        case "AddRooms":
-                        case "AddCustomers":
-                        case "AddCustomerID":
-                        case "DeleteFlight":
-                        case "DeleteCars":
-                        case "DeleteRooms":
-                        case "DeleteCustomer":
-                        case "QueryFlight":
-                        case "QueryCars":
-                        case "QueryRooms":
-                        case "QueryFlightPrice":
-                        case "QueryCarsPrice":
-                        case "QueryRoomsPrice":
-                        case "ReserveFlight":
-                        case "ReserveRoom":
-                        case "ReserveCar":
-                        case "bundle":
+                        }
+
+                    }
+                    else if (method.contains("Customer")) {
+                        switch (method) {
+                            case "AddCustomers":
+                                int newId = middleware.newCustomer(jsob.getInt("xid"));
+                                os = clientSocket.getOutputStream();
+                                os.write(("Added customer with ID " + newId + ".").getBytes());
+                                os.flush();
+                                os.close();
+                                break;
+                            case "AddCustomerID":
+                                boolean succesfullyCreated = middleware.newCustomer(jsob.getInt("xid"), jsob.getInt("customerID"));
+                                os = clientSocket.getOutputStream();
+                                if (succesfullyCreated) {
+                                    os.write(("Added customer with ID " + jsob.getInt("customerID") + ".").getBytes());
+                                } else {
+                                    os.write(("Customer with ID " + jsob.getInt("customerID") + " already exists.").getBytes());
+                                }
+                                os.flush();
+                                os.close();
+                                break;
+                            case "DeleteCustomer":
+                                boolean succesfullyDeleted = middleware.newCustomer(jsob.getInt("xid"), jsob.getInt("customerID"));
+                                os = clientSocket.getOutputStream();
+                                if (succesfullyDeleted) {
+                                    os.write(("Deleted customer with ID " + jsob.getInt("customerID") + ".").getBytes());
+                                } else {
+                                    os.write(("Customer with ID " + jsob.getInt("customerID") + " does not exist.").getBytes());
+                                }
+                                os.flush();
+                                os.close();
+                                break;
+                        }
                     }
                 } catch (Exception ioe) {
                     System.out.println("Exception encountered on accept. Ignoring. Stack Trace :");
                     ioe.printStackTrace();
                 }
             }
-        }
-        catch (Exception e) {
-            System.err.println((char)27 + "[31;1mServer exception: " + (char)27 + "[0mUncaught exception");
+        } catch (
+                Exception e)
+
+        {
+            System.err.println((char) 27 + "[31;1mServer exception: " + (char) 27 + "[0mUncaught exception");
             e.printStackTrace();
             System.exit(1);
         }
 
         // Create and install a security manager
         if (System.getSecurityManager() == null)
+
         {
             System.setSecurityManager(new SecurityManager());
         }
+
     }
 
-    public TCPMiddleware(String name, IResourceManager rm1, IResourceManager rm2, IResourceManager rm3)
-    {
+    public TCPMiddleware(String name, IResourceManager rm1, IResourceManager rm2, IResourceManager rm3) {
         super(name);
     }
 }
